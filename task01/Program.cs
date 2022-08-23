e794f094dcb8086e27bdc76936bad7aa0067f8e0
Panov Timofey <kirito.panov57@Gmail.com>
2022-08-23 21:40:09 +0300

    Формирование нового массива из строк длинов  в 3 символа

---
+++ task01/Program.cs
@@ -0,0 +1,40 @@
+﻿/* Напишите прграмму, которая из имеющегося массива строк формирует массив из строк, длина которых
+   меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, либо задать на старте
+   выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись
+   исключительно массивами.
+   ["hello", "2", "world", ":-)"] -> ["2", ":-)"]
+*/
+namespace FinalTask01
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            string[] FirstArray = new string[7] { "1234", "1567", "-2", ":-)", "var", "nike", "Africa" };
+            string[] SecondArray = new string[FirstArray.Length];
+            
+            SecondArrayWithIF(FirstArray, SecondArray);
+            PrintArray(SecondArray);
+        }
+        static void SecondArrayWithIF(string[] FirstArray, string[] SecondArray)
+            {
+                int count = 0;
+                for (int i = 0; i < FirstArray.Length; i++)
+                {
+                    if (FirstArray[i].Length <= 3)
+                    {
+                        SecondArray[count] = FirstArray[i];
+                        count++;
+                    }
+                }
+            }
+           static void PrintArray(string[] array)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.Write($"{array[i]} ");
+                }
+                Console.WriteLine();
+            }
+    }
+}
